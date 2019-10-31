b44103c1ce40c145e9cd8fd24d57139bde945f20
Isaak Johnson <isaak.johnson@aggiemail.usu.edu>
2019-10-30 20:22:43 -0600

    when you place a bid, the models get updated

--- auction_app/admin.py
+++ auction_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import SilentItem, Bid
+from .models import SilentItem, Bid, User
 
 admin.site.register(SilentItem)
 admin.site.register(Bid)
+admin.site.register(User)
