f369e6357d94365a90828061c902538372de4e4d
Benjamin Taylor <benjamin.taylor1234@aggiemail.usu.edu>
2019-11-12 20:47:51 -0700

    Created some tests for users that don't all work

--- auction_app/tests.py
+++ auction_app/tests.py
@@ -1,6 +1,34 @@
 from django.test import TestCase
-from .forms import *
+from django.urls import reverse
 
+# from auction_app.models import *
+# from .forms import *
+
+class CreateAccountTest(TestCase):
+    def setUp(self):
+        init_test_db()
+
+# Create your tests here.
+class UsersViewTest(TestCase):
+    @classmethod
+    def setup(self):
+        init_test_db()
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('users'))
+        self.assertRedirects(response, '/login/?next=/users')
+
+    def test_view_url_exists_at_desired_location(self):
+        login = self.client.login(username='admin', password='letmepass')
+        print(login)
+        response = self.client.get(reverse('users'))
+        print(response)
+        self.assertEqual(str(response.context['user']), 'admin')
+        self.assertEqual(response.status_code, 200)
+
+    def test_one_plus_one_equals_two(self):
+        print("Method: test_one_plus_one_equals_two.")
+        self.assertEqual(1 + 1, 2)
 
 # helper function to set up databse
 def init_test_db():
@@ -37,9 +65,9 @@ def init_test_db():
     liveAuction.save()
     for i in range(10):
         item = SilentItem(
-            title=randomString(), 
-            description=randomString(), 
-            imageName=randomString(), 
+            title=randomString(),
+            description=randomString(),
+            imageName=randomString(),
             auction=silentAuction
         )
         item.save()
@@ -63,9 +91,3 @@ def nukeDB():
     AuctionUser.objects.all().delete()
     # BidSilent.objects.all().delete()
     # BidLive.objects.all().delete()
-
-
-
-class CreateAccountTest(TestCase):
-    def setUp(self):
-        init_test_db()
